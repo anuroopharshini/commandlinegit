ef7e433a9590f35ff58df3c0106089fb77275b0e
anuroopharshini <pandiri.harshini@gmail.com>
2021-10-27 18:56:51 -0700

    hello first commit

---
+++ src/test/java/test/test.java
@@ -0,0 +1,27 @@
+package test;
+import org.testng.annotations.*;
+
+public class test {
+	
+	@Test
+	public void m1() {
+		
+		System.out.println("hello");
+	}
+	
+	@Test
+	public void m2() {
+		
+		System.out.println("hello harshini");
+	}
+	
+	@Test
+	public void m3() {
+		
+		System.out.println("hello amogha anuroop");
+	}
+	
+	
+	
+	
+}
